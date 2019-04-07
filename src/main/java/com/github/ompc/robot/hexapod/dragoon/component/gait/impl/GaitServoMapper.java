1b5912e8a6b0768b97a0a912f24de190013af182
杜琨 <dukun@alibaba-inc.com>
2019-04-07 14:22:15 +0800

    测试

--- src/main/java/com/github/ompc/robot/hexapod/dragoon/component/gait/impl/GaitServoMapper.java
+++ src/main/java/com/github/ompc/robot/hexapod/dragoon/component/gait/impl/GaitServoMapper.java
@@ -49,24 +49,24 @@ class GaitServoMapper {
             = new HashMap<>();
 
     public GaitServoMapper() {
-        gaitServoMap.put(new Key(R_F, HIP), 1);
-        gaitServoMap.put(new Key(R_F, KNE), 2);
-        gaitServoMap.put(new Key(R_F, ANK), 3);
-        gaitServoMap.put(new Key(R_M, HIP), 4);
-        gaitServoMap.put(new Key(R_M, KNE), 5);
-        gaitServoMap.put(new Key(R_M, ANK), 6);
-        gaitServoMap.put(new Key(R_H, HIP), 7);
-        gaitServoMap.put(new Key(R_H, KNE), 8);
-        gaitServoMap.put(new Key(R_H, ANK), 9);
-        gaitServoMap.put(new Key(L_F, HIP), 16);
-        gaitServoMap.put(new Key(L_F, KNE), 17);
-        gaitServoMap.put(new Key(L_F, ANK), 18);
-        gaitServoMap.put(new Key(L_M, HIP), 13);
-        gaitServoMap.put(new Key(L_M, KNE), 14);
-        gaitServoMap.put(new Key(L_M, ANK), 15);
-        gaitServoMap.put(new Key(L_H, HIP), 10);
-        gaitServoMap.put(new Key(L_H, KNE), 11);
-        gaitServoMap.put(new Key(L_H, ANK), 12);
+        gaitServoMap.put(new Key(L_H, HIP), 1);
+        gaitServoMap.put(new Key(L_H, KNE), 2);
+        gaitServoMap.put(new Key(L_H, ANK), 3);
+        gaitServoMap.put(new Key(L_M, HIP), 4);
+        gaitServoMap.put(new Key(L_M, KNE), 5);
+        gaitServoMap.put(new Key(L_M, ANK), 6);
+        gaitServoMap.put(new Key(L_F, HIP), 7);
+        gaitServoMap.put(new Key(L_F, KNE), 8);
+        gaitServoMap.put(new Key(L_F, ANK), 9);
+        gaitServoMap.put(new Key(R_F, HIP), 16);
+        gaitServoMap.put(new Key(R_F, KNE), 17);
+        gaitServoMap.put(new Key(R_F, ANK), 18);
+        gaitServoMap.put(new Key(R_M, HIP), 13);
+        gaitServoMap.put(new Key(R_M, KNE), 14);
+        gaitServoMap.put(new Key(R_M, ANK), 15);
+        gaitServoMap.put(new Key(R_H, HIP), 10);
+        gaitServoMap.put(new Key(R_H, KNE), 11);
+        gaitServoMap.put(new Key(R_H, ANK), 12);
     }
 
     public Integer getServoIndex(Leg leg, Joint joint) {
